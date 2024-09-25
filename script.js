dfe8e59e38ab7f6e474589d091d2a256d2acd8e9
Michael Rodriguez <apple.mrodriguez956@gmail.com>
2024-09-24 22:35:03 -0500

    created list toggle on click for Date cell

--- script.js
+++ script.js
@@ -1,30 +1,30 @@
-const addTime =  document.querySelector('.add-time');
-const addButton = document.querySelector('#addButton');
+const addTime =  document.querySelector('.time-form'); 
+const addButton = document.querySelector('.add-button');
 const clearButton = document.querySelector('#clear-button');
-const inputField = document.querySelector('#inputTime');
+const inputField = document.querySelector('.input-field');
 
-const minutes = JSON.parse(localStorage.getItem('minutes')) || [];
+const minutes = JSON.parse(localStorage.getItem('minutes')) || []; //grab object array from localStorage OR create an empty array
 
 
-let taskCount = minutes.length;
-console.log(taskCount);
-addButton.addEventListener('click', addMinutes);
-clearButton.addEventListener('click', clearLocalStorage);
+let taskCount = minutes.length; //calculate task count by length of minutes array
+//console.log(taskCount);
+addButton.addEventListener('click', addMinutes); //run function addMinutes on button click
+clearButton.addEventListener('click', clearLocalStorage); //clear localStorage when button is pressed
 
 window.onload = function() {
-    updateTable();
+    updateTable(); //ensure latest items are pulled from localStorage and displayed on load
   };
 
 
 function addMinutes(e)
 {
-    e.preventDefault();
+    e.preventDefault(); //prevent default button behavior
 
-    const inputTime = document.querySelector('#inputTime').value;
+    const inputTime = document.querySelector('input.input-field').value; //get minutes value from the input field
 
 
 
-    const constraints = {
+    const constraints = { //validation for minutes entry using validate.js
         inputTime: {
             presence: true,
             numericality: {
@@ -45,63 +45,56 @@ function addMinutes(e)
 
     else{
 
-        taskCount += 1;
+        taskCount += 1; //if validation passes increase taskCount
     }
 
 
-/*    if (!inputTime || inputTime < 0 || inputTime > 120) 
-        {
-        alert("Please enter a valid number between 0 and 120.");
-        return;
-      }
-
- */   
-
-
-
-    const newEntry = {
+    const newEntry = { //save data into new object
         date: saveDate().toString(),
         minutes: inputTime,
-        taskNumber: taskCount
+        taskNumber: taskCount 
     };
 
     
    
-    minutes.push(newEntry);
+    minutes.push(newEntry); //push new object into array
 
     
 
-    document.querySelector('#inputTime').value = ""
+    document.querySelector('.input-field').value = "" //reset input field
 
     
-    updateTable();
-    localStorage.setItem('minutes', JSON.stringify(minutes));
+    updateTable(); //run updateTable function to display updated table
+    localStorage.setItem('minutes', JSON.stringify(minutes)); //stringify minutes array and set as item 'minutes' in localStorage
 
 
 }
 
 function updateTable() 
 {
-    const userTable = document.querySelector('#user-table tbody');
-    const tableFoot = document.querySelector('#user-table tfoot')
+    const userTable = document.querySelector('#user-table tbody'); //grab table body where user data will be displayed
+    const tableFoot = document.querySelector('#user-table tfoot'); //grab table foot where total minutes will be displayed
+    const dateCell = document.createElement('td');
 
-    let previousDate = null;
-    userTable.innerHTML = '';
-    tableFoot.innerHTML = '';
-    let total = 0;
+    let previousDate = null; //re-initialize variable previousDate when updateTable() is ran
+    userTable.innerHTML = ''; //clear the table body
+    tableFoot.innerHTML = ''; //clear table footer
+    let total = 0; //reset total
 
-    minutes.forEach(entry => {
+    minutes.forEach(entry => { //loop through minutes array using each object as an entry
 
-        if (entry.date !== previousDate)
+        if (entry.date !== previousDate) //if the date for an entry is not equal to the previousDate, create a date row
             {
                 const dateRow = document.createElement('tr');
-                const dateCell = document.createElement('td');
+                
                 dateRow.classList.add('date-row','table-secondary');
                 
-                dateCell.textContent = entry.date;
+                dateCell.innerHTML = "<button class='btn toggle-list' type='button' data-bs-toggle='collapse' data-bs-target='#collapseExample'>" + entry.date + "</button>"
+                //dateCell.textContent = entry.date;
                 dateRow.appendChild(dateCell);
                 userTable.appendChild(dateRow);
                 dateCell.setAttribute('colspan', 3);
+    
                 previousDate = entry.date;
 
             }
@@ -109,26 +102,29 @@ function updateTable()
          total += Number(entry.minutes);
          console.log("total:" + total);
 
-        const newRow = document.createElement('tr');   
-        const taskNumberCell = document.createElement('td');
-        const minutesCell = document.createElement('td');
- 
-
-        console.log(entry.taskNumber);
+    
+        const newRow = document.createElement('tr');
+        newRow.classList.add("table-data");
         
-        taskNumberCell.textContent = entry.taskNumber;
-        minutesCell.textContent = entry.minutes;
-     //   totalCell.textContent = total;
+       
+        
+        newRow.innerHTML = "<td class='w-50'>" + entry.taskNumber + "</td><td class=''>" + 
+        entry.minutes + "</td>"; 
 
-        newRow.appendChild(taskNumberCell);
-        newRow.appendChild(minutesCell);
-     //   newRow.appendChild(totalCell);
         userTable.appendChild(newRow);
-
-  
-
         
     });
+    
+    const tableData = document.querySelectorAll('.table-data');
+    const toggleList = document.querySelector('.toggle-list');
+
+
+    toggleList.addEventListener('click', function() {
+        tableData.forEach(element => {
+          element.classList.toggle('d-none');
+        });
+      });
+
 
     const totalRow = document.createElement('tr');
     const totalCell = document.createElement('td');
@@ -141,6 +137,8 @@ function updateTable()
 
     totalRow.appendChild(totalCell);
     tableFoot.appendChild(totalRow);
+
+    tableFoot.classList.add("total-row");
 
 
 
@@ -163,3 +161,13 @@ function clearLocalStorage() {
     window.location.reload();
   }
 
+function hideTable(displayed) 
+{
+ if(displayed.style.display === "none") {
+    displayed.style.display = "block";
+ } 
+ else {
+    displayed.style.display = "none";
+ }
+    
+}
