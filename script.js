3c7990e391856de63abbcc533195e954497b330c
Michael Rodriguez <apple.mrodriguez956@gmail.com>
2024-10-01 10:31:00 -0500

    added individual collapse functionality to date rows

--- script.js
+++ script.js
@@ -7,12 +7,15 @@ const minutes = JSON.parse(localStorage.getItem('minutes')) || []; //grab object
 
 
 let taskCount = minutes.length; //calculate task count by length of minutes array
+let dateCount = 0;
+
 //console.log(taskCount);
 addButton.addEventListener('click', addMinutes); //run function addMinutes on button click
 clearButton.addEventListener('click', clearLocalStorage); //clear localStorage when button is pressed
 
 window.onload = function() {
     updateTable(); //ensure latest items are pulled from localStorage and displayed on load
+    dateToggle();
   };
 
 
@@ -65,6 +68,7 @@ function addMinutes(e)
 
     
     updateTable(); //run updateTable function to display updated table
+    dateToggle();
     localStorage.setItem('minutes', JSON.stringify(minutes)); //stringify minutes array and set as item 'minutes' in localStorage
 
 
@@ -85,15 +89,17 @@ function updateTable()
 
     minutes.forEach(entry => { //loop through minutes array using each object as an entry
 
+        
         if (entry.date !== previousDate) //if the date for an entry is not equal to the previousDate, create a date row
             {
+                dateCount += 1;
                 const dateRow = document.createElement('tr');
                 
                 dateRow.classList.add('date-row','table-secondary'); //add classes to the dateRow table row element
                 
 
                 const dateCell = document.createElement('td');
-                dateCell.innerHTML = "<button class='btn toggle-list' type='button'>" + entry.date + "</button>" //create clickable table cell that contains date of entries
+                dateCell.innerHTML = `<button class='btn toggle-list-${dateCount}' type='button'>` + entry.date + "</button>" //create clickable table cell that contains date of entries
                 //dateCell.textContent = entry.date;
                 dateRow.appendChild(dateCell); // add date cell to the dateRow
                 userTable.appendChild(dateRow); //add date Row to table 
@@ -108,7 +114,7 @@ function updateTable()
 
     
         const newRow = document.createElement('tr');
-        newRow.classList.add("table-data");
+        newRow.classList.add(`table-data-${dateCount}`);
         
        
         
@@ -119,16 +125,6 @@ function updateTable()
         
     });
     
-    const tableData = document.querySelectorAll('.table-data');
-    const toggleList = document.querySelector('.toggle-list');
-
-
-    toggleList.addEventListener('click', function() {
-        tableData.forEach(element => {
-          element.classList.toggle('d-none');
-        });
-      });
-
 
     const totalRow = document.createElement('tr');
     const totalCell = document.createElement('td');
@@ -164,3 +160,23 @@ function clearLocalStorage() {
     localStorage.clear();
     window.location.reload();
   }
+
+
+function dateToggle() {
+
+    console.log("dateCount: " + dateCount);
+      for (let i = 0; i <= dateCount; i++) {
+
+        const toggleList = document.querySelector(`.toggle-list-${i}`);
+        if(toggleList)
+            {
+            toggleList.addEventListener('click', function() {
+                const tableData = document.querySelectorAll(`.table-data-${i}`);
+                tableData.forEach(element => {
+                  element.classList.toggle('d-none');
+                });
+              });
+        }
+
+    }
+}
